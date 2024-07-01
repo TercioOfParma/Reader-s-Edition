6ec739057073f7e680472995b6d7637c6408abd9
Elliot Lake <elliotlake@protonmail.com>
2024-07-01 19:27:02 +0100

    Add basic tests

--- ReadersEdition.Tests/DocumentTests.cs
+++ ReadersEdition.Tests/DocumentTests.cs
@@ -1,15 +1,28 @@
+using ReadersEdition.Domain;
+
 namespace ReadersEdition.Tests;
 
 public class DocumentTests
 {
+    public Document Doc {get; set;}
     [SetUp]
     public void Setup()
     {
+        Doc = new Document("This*is,A.Proper!Test;For Vocab", true, 30);
     }
 
     [Test]
-    public void Test1()
+    public void ProducesCorrectGlossSize()
     {
-        Assert.Pass();
+        if(Doc.Glosses.Count == 7)
+            Assert.Pass();
+        Assert.Fail();
+    }
+    [Test]
+    public void ProducesCorrectFrequencyDictionarySize()
+    {
+        if(Doc.FrequencyInDocument.Count == 7)
+            Assert.Pass();
+        Assert.Fail();
     }
 }
